e76c7637df09537f0326d040a5a1d43a9f8e1747
Ariel Quinones <arielqns@gmail.com>
2019-10-23 13:36:05 -0400

    Added POST on router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,29 +7,30 @@ var burger = require('../models/burger.js');
 var burger = require('../models/burger.js');
 
 // Create the routes and associated logic
+// GET
 router.get('/', function(req, res) {
   burger.all(function(burger_data) {
     console.log(burger_data);
-    res.render('index');
+    res.render('index',{burger_data});
   })
 })
+// PUT
+router.put('/burgers/update', function(req,res){
+    burger.update(req.body.burger_id, function(result){
+        console.log(result);
+        res.redirect('/');
+    });
+});
+//POST
+router.post('/burgers/create', function(req, res) {
+  burger.create(req.body.burger_name)
 
-// router.post('/burgers', function(req, res) {
-//   burger.insertOne([
-//     'burger_name'
 //   ], [
 //     req.body.burger_name
 //   ], function(data) {
 //     res.redirect('/');
 //   });
-// });
-
-router.put('/burgers/update', function(req, res) {
-  burger.update(req.body.burger_id, function(result){
-      console.log(result);
-        res.redirect('/');
-  });
-});
+})
 
 // Export routes for server.js to use.
 module.exports = router;
