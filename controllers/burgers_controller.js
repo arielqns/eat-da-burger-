791c0f4aa7fda755ad8bbd0e43e5607a029d5d44
Ariel Quinones <arielqns@gmail.com>
2019-10-24 14:03:11 -0400

    added image & footer to main

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,8 +12,8 @@ router.get('/', function(req, res) {
   burger.all(function(burger_data) {
     console.log(burger_data);
     res.render('index',{burger_data});
-  })
-})
+  });
+});
 // PUT
 router.put('/burgers/update', function(req,res){
     burger.update(req.body.burger_id, function(result){
@@ -22,15 +22,12 @@ router.put('/burgers/update', function(req,res){
     });
 });
 //POST
-router.post('/burgers/create', function(req, res) {
-  burger.create(req.body.burger_name)
-
-//   ], [
-//     req.body.burger_name
-//   ], function(data) {
-//     res.redirect('/');
-//   });
-})
+router.post('/burgers', function(req, res) {
+  burger.create(req.body.burger_name), function(data) {
+      console.log(data);
+    res.redirect('/');
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -6,15 +6,9 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	burger_name varchar '(255)' NOT NULL,
 	devoured BOOLEAN DEFAULT false,
   	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   	dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   	PRIMARY KEY(id)
 );
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Blue- Cheese Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
